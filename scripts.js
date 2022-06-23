a10516da0fd135b911670eedf56c9183862ba702
Isadora Farache <FaracheDev@gmail.com>
2022-06-22 22:52:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
- //Existem diferentes tipos de caixa de diálogo, são elas: alerta, confirmação e prompt de entrada.//
-        //Criei uma var "nome" para armazenar o nome que a pessoa digitar//
+ /*
+ Existem diferentes tipos de caixa de diálogo, são elas: alerta, confirmação e prompt de entrada.
+ Criei uma var "nome" para armazenar o nome que a pessoa digitar.
+ */
 
         alert("Olá. Tudo bom?");
         var nome = prompt("Qual o seu nome?");
